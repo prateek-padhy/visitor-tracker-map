721b0e7ccb2e59487a0d50138cbd466658fe89a3
prateek-padhy <58298248+prateek-padhy@users.noreply.github.com>
2025-02-25 23:10:26 +0000

    - improve map control

--- src/components/Map/VisitorControl.tsx
+++ src/components/Map/VisitorControl.tsx
@@ -5,17 +5,19 @@ const StyledVisitorControl = styled.div`
     bottom: 0;
     right: 0;
     margin-right: 10px;
-    margin-bottom: 24px;
+    margin-bottom: 40px;
     display: flex;
     flex-direction: column;
     background-color: rgba(255, 255, 255, 1);
     border-radius: 4px;
+    box-shadow: 0 0 0 2px rgba(0,0,0,.1);
 `;
 
 const StyledControlButton = styled.button`
   background-color: rgba(255, 255, 255, 1);
   border-radius: 4px;
   padding: 8px;
+  font-size: 16px;
 `;
 
 type VisitorControlProps = {
