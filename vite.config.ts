e205ed0a0658607a9afabfc032e7e31a60dc17c4
prateek-padhy <58298248+prateek-padhy@users.noreply.github.com>
2025-03-08 22:28:15 +0000

    - update repo name

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
-  base: '/vite-react-test/', 
+  base: '/visitor-tracker-map/', 
   build: {
     outDir: "dist"
   },
